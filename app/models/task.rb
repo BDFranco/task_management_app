a8f58ed8d403b8b1638701e46ea16db113f828e0
Tyryll <104739595+Tyryll@users.noreply.github.com>
2025-03-19 11:56:06 +0800

    Fix lint errors using rubocop (#9)
    * Added user and project relationship
    
    * Task Controller
    
    * Feature/task model (#7)
    
    * task model creation
    
    * fixed errors
    
    * Added task relationship
    
    * Fix lint errors using rubocop
    
    ---------
    
    Co-authored-by: glophelyn <glophelynjocson00@gmail.com>
    Co-authored-by: Jumer <jumerdelatorre0@gmail.com>

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
+    # Relationships
+    belongs_to :project
+    belongs_to :assignee, class_name: "User", foreign_key: :assigned_to
+
     # Validations
     validates :title, presence: true
     validates :description, presence: true

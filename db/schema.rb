d158ca88be8369e58eccf7036177dd9c5f6ebfbb
Jumer <jumerdelatorre0@gmail.com>
2025-03-19 11:22:05 +0800

    Feature/task model (#7)
    * task model creation
    
    * fixed errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_18_053200) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_19_022857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_18_053200) do
     t.string "name"
     t.text "description"
     t.integer "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "status"
+    t.datetime "due_date"
+    t.string "assigned_to"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

9882500f464c5fd152cfbd0c99253a2dd1ac5ea2
Jumer <jumerdelatorre0@gmail.com>
2025-03-19 11:22:05 +0800

    Feature/task model (#7)
    * task model creation
    
    * fixed errors

---
+++ db/migrate/20250319022857_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.string :status
+      t.datetime :due_date
+      t.string :assigned_to
+
+      t.timestamps
+    end
+  end
+end

e4dd4f32b7f5f3f445bad9054e5161c2809152a1
BDFranco <francobyron432@gmail.com>
2025-03-19 13:04:33 +0800

    Added Status Field

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+    # Statuses
+    STATUSES = [ "pending", "in_progress", "completed" ]
     # Relationships
     belongs_to :project
     belongs_to :assignee, class_name: "User", foreign_key: :assigned_to
@@ -6,7 +8,20 @@ class Task < ApplicationRecord
     # Validations
     validates :title, presence: true
     validates :description, presence: true
-    validates :status, presence: true
+    validates :status, presence: true, inclusion: { in: STATUSES }
     validates :due_date, presence: true
     validates :assigned_to, presence: true
+
+    # Define
+    def pending?
+        status == "pending"
+    end
+
+    def in_progress?
+        status == "in_progress"
+    end
+
+    def completed?
+        status == "completed"
+    end
 end

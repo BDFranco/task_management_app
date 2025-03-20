afddb4eae0a3e977b33a452264760ffa3ffaa6e2
BDFranco <francobyron432@gmail.com>
2025-03-20 13:28:23 +0800

    Fix test error

--- db/migrate/20250319080819_add_project_id_to_tasks.rb
+++ db/migrate/20250319080819_add_project_id_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddProjectIdToTasks < ActiveRecord::Migration[8.0]
   def change
-    t.references :project, null: true, foreign_key: true
+    add_reference :tasks, :project, null: false, foreign_key: true
   end
 end

c3073d992b5314787f1f01d1fefe55e3ba5a7edf
BDFranco <francobyron432@gmail.com>
2025-03-20 13:19:49 +0800

    Added Homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_19_080819) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_20_051421) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.integer "created_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "creator_id"
-    t.index ["creator_id"], name: "index_projects_on_creator_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -44,10 +43,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_19_080819) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role", default: "developer"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "projects", "users", column: "creator_id"
   add_foreign_key "tasks", "projects"
 end
